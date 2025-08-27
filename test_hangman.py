d8f09fd00cc124f08a0bbd0d801ccc48645db289
Dipesh Wagle <deepacewagle@gmail.com>
2025-08-27 19:40:10 +0930

    Added Test case for intermediate words

--- test_hangman.py
+++ test_hangman.py
@@ -12,5 +12,13 @@ class TestHangman(unittest.TestCase):
         word = self.game.choose_word("1")
         self.assertIn(word, WORDS)
         self.assertEqual(word, WORDS[0])
+
+    def test_choose_word_intermediate(self):
+        """Test intermediate level phrase selection"""
+        random.choice = lambda x: x[-1]  # Mock to return last phrase
+        phrase = self.game.choose_word("2")
+        self.assertIn(phrase, PHRASES)
+        self.assertEqual(phrase, PHRASES[-1])
+        
 if __name__ == "__main__":
     unittest.main()
